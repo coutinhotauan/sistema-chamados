132769f19081d6ab795e47908e71d3a80d65f949
Tauan Coutinho <tauan_coutinho1@hotmail.com>
2022-03-08 12:08:57 -0300

    tela de login pronta

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,41 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signin.css';
+import logo from '../../assets/logo.png';
 
 function SignIn() {
-    return (
-      <div>
-        <h1>Pagina Login</h1>
-      </div>
-    );
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
   }
-  
-  export default SignIn;
+
+  return (
+    <div className='container-center'>
+    
+      <div className='login'>
+
+        <div className='login-area'>
+
+          <img src={logo} alt="Sistema logo"/>
+
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => { setEmail(e.target.value) } }/>
+          <input type="password" placeholder='******' value={password} onChange={ (e) => { setPassword(e.target.value) } } />
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default SignIn;
