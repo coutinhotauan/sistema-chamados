74c1cea105aee4696a8d4f6924e7d3ba0b70484a
Tauan Coutinho <tauan_coutinho1@hotmail.com>
2022-03-08 12:17:28 -0300

    tela de cadastro criada

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,42 @@
-
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 function SignUp() {
-    return (
-      <div>
-        <h1>Pagina de cadastro</h1>
-      </div>
-    );
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
   }
-  
-  export default SignUp;
+
+  return (
+    <div className='container-center'>
+    
+      <div className='login'>
+
+        <div className='login-area'>
+
+          <img src={logo} alt="Sistema logo"/>
+
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar uma conta</h1>
+          <input type="text" placeholder="Seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => { setEmail(e.target.value) } }/>
+          <input type="password" placeholder='******' value={password} onChange={ (e) => { setPassword(e.target.value) } } />
+          <button type='submit'>Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui uma conta? Entre</Link>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default SignUp;
